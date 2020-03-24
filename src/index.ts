62a721b2365c15d75bfb512fc80de38e29158db3
Muhammad Umair <mumairofficial@gmail.com>
2020-03-25 02:56:12 +0500

    chore: clean up

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ require('dotenv').config();
 import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-import { schema } from './graphql';
+import { resolvers, typeDefs } from './graphql';
 import { connectDatabase } from './database';
 
 const port = process.env.PORT;
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
-  const apolloServer = new ApolloServer({ schema, context: () => ({ db }) });
+  const apolloServer = new ApolloServer({ resolvers, typeDefs, context: () => ({ db }) });
 
   apolloServer.applyMiddleware({ app, path: '/api' });
 
